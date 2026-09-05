f7fb7d8ffe71d548deb60f063be3205390e127cf
greskysoul <sirisgrantham@163.com>
2026-09-05 10:41:54 +0800

    ci: 固定 ADS 至 4.5.0(Qt5 兼容) 并修复 PowerShell -j 参数解析

--- ci/ads_version.h
+++ ci/ads_version.h
@@ -1,8 +1,8 @@
 #ifndef ads_versionH
 #define ads_versionH
-#define ADS_VERSION_MAJOR 5
-#define ADS_VERSION_MINOR 1
-#define ADS_VERSION_PATCH 1
+#define ADS_VERSION_MAJOR 4
+#define ADS_VERSION_MINOR 5
+#define ADS_VERSION_PATCH 0
 #define ADS_VERSION ADS_VERSION_CHECK(ADS_VERSION_MAJOR, ADS_VERSION_MINOR, ADS_VERSION_PATCH)
 #define ADS_VERSION_CHECK(major, minor, patch) ((major<<16) | (minor<<8) | (patch))
 #endif // ads_versionH
